338ccf0f75dfcc38c03d116d86cd1534bc6a43ec
knull <kelanidarasimi9@gmail.com>
2025-01-07 06:22:56 +0100

    stupid program

--- excercise/chapter1/HelloWorld/hello_world.go
+++ excercise/chapter1/HelloWorld/hello_world.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 // a simple hello world program to open up my 57 exercersice
 // for a programmer
 
 func main() {
-	fmt.Printf("What is your name ?: ")
+	fmt.Printf("What is your name?: ")
 	var input string
 	_, err := fmt.Scan(&input)
 
@@ -14,7 +18,21 @@ func main() {
 		fmt.Println("Please enter a valid name")
 		return
 	}
-
 	fmt.Printf("Hello, %s nice to meet you!\n", input)
 
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Press 0")
+	var text string
+
+	// just a stupid pro
+outerLoop:
+	for scanner.Scan() {
+		text = scanner.Text()
+		switch {
+		case text == "0":
+			break outerLoop
+		default:
+			fmt.Println("Press 0")
+		}
+	}
 }
